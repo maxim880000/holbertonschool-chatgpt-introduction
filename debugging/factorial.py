da081bf1eb1e4663a7c353cee8f21cda58ee3c70
maxim880000 <mdholberton@gmail.com>
2026-01-12 09:38:39 +0100

    committed last version of 0. Debugging - Python Factorial with comment

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
+def factorial(n): # create factorial function 
+    result = 1 # variable creation 
+    while n > 1: #loops
+        result *= n # result = result * n keep the new result
+        n -= 1 # remove 1 to n (so that the program stops )
     return result
 
 f = factorial(int(sys.argv[1]))
-print(f)
+print(f) # On affiche le résultat à l'écran
