1f67528407559faa12739d4680b2efcb74aa51b5
maxim880000 <mdholberton@gmail.com>
2026-01-12 10:29:59 +0100

    committed 1. Debugging - Python Arguments with comment

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -2,4 +2,7 @@
 import sys
 
 for i in range(1, len(sys.argv)):
-    print(sys.argv[i])
+# On commence à 1 pour **ignorer le nom du fichier**
+# len(sys.argv) = nombre total d'éléments (nom du fichier + arguments)
+	print(sys.argv[i])
+	# Affiche l'argument numéro i
